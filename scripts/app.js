60a1fd7c1a478e317257c65bb3f507109ed9f9b5
lp_dev <lenyiepromise@gmail.com>
2025-06-30 13:06:09 +0100

    changing image format

--- scripts/app.js
+++ scripts/app.js
@@ -1,14 +1,3 @@
-// Setting the image to each slide
-let slide1 = document.getElementById("slide1");
-let slide2 = document.getElementById("slide2");
-let slide3 = document.getElementById("slide3");
-let slide4 = document.getElementById("slide4");
-
-slide1.src = "img/slider/slide1.png";
-slide2.src = "img/slider/slide2.png";
-slide3.src = "img/slider/slide3.png";
-slide4.src = "img/slider/slide4.png";
-
 let slides = document.querySelectorAll('.slide');
 let dots = document.querySelectorAll('.dot');
 let current = 0;
